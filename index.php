61caa7f374eebb5974256ca99e6d3590e53c48cb
dlirA01 ♡ <arild.edvin.andreassen@gmail.com>
2021-01-29 10:00:16 +0100

    Added index.html & style.css

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="no">
+<head>
+    <!-- Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Alien">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- ======= -->
+    <!-- General -->
+    <title>Alien</title>
+    <link rel="icon" href="">
+    <!-- ===== -->
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <!-- ======= -->
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.4.2/gsap.min.js"></script>
+    <script src="https://unicons.iconscout.com/release/v3.0.6/script/monochrome/bundle.js"></script>
+    <script src="javascript/rellax.min.js"></script>
+    <script src="javascript/rellax.js"></script>
+    <!-- =========== -->
+    <!-- Stylesheets -->
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1462889/unicons.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/solid.css">
+</head>
+<body>
+
+    <main class="flexbox">
+
+        <h1>Hallo verden!</h1>
+
+    </main>
+
+</body>
+</html>
